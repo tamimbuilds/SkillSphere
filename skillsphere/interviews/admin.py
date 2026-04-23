77806db6e07b445256cc703419cd69eb33e9ddf0
ShirinAkter56 <23201056@uap-bd.edu>
2026-04-23 10:54:43 +0600

    commit

--- skillsphere/interviews/admin.py
+++ skillsphere/interviews/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Interview, Interviewer, Shortlist
 
 # Register your models here.
+
+@admin.register(Interviewer)
+class InterviewerAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'designation']
+
+
+@admin.register(Shortlist)
+class ShortlistAdmin(admin.ModelAdmin):
+    list_display = ['candidate', 'job', 'shortlisted_at']
+
+
+@admin.register(Interview)
+class InterviewAdmin(admin.ModelAdmin):
+    list_display = ['candidate', 'job', 'interview_type', 'status', 'score']
+
+
+admin.site.register(Interviewer)
+admin.site.register(Shortlist)
+admin.site.register(Interview)
