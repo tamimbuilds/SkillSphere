8f718fdc8e88b22d90e67d9e4c22bfbeb125d0b8
tamimlikhon <tamimlikhon456@gmail.com>
2026-05-09 12:25:38 +0600

    alter

--- skillsphere/interviews/migrations/0004_alter_interview_scheduled_time.py
+++ skillsphere/interviews/migrations/0004_alter_interview_scheduled_time.py
@@ -12,6 +12,10 @@ TIME_FORMATS = [
 
 
 def normalize_scheduled_times(apps, schema_editor):
+    if schema_editor.connection.vendor == "postgresql":
+        normalize_scheduled_times_postgres(schema_editor)
+        return
+
     Interview = apps.get_model("interviews", "Interview")
 
     for interview in Interview.objects.all().only("pk", "scheduled_time"):
@@ -30,6 +34,29 @@ def normalize_scheduled_times(apps, schema_editor):
 
         interview.scheduled_time = normalized_time.strftime("%H:%M:%S")
         interview.save(update_fields=["scheduled_time"])
+
+
+def normalize_scheduled_times_postgres(schema_editor):
+    with schema_editor.connection.cursor() as cursor:
+        cursor.execute(
+            """
+            UPDATE interviews_interview
+            SET scheduled_time = CASE
+                WHEN scheduled_time IS NULL OR btrim(scheduled_time) = '' THEN '09:00:00'
+                WHEN btrim(scheduled_time) ~ '^[0-9]{1,2}:[0-9]{2}:[0-9]{2}$'
+                    THEN to_char(btrim(scheduled_time)::time, 'HH24:MI:SS')
+                WHEN btrim(scheduled_time) ~ '^[0-9]{1,2}:[0-9]{2}$'
+                    THEN to_char(btrim(scheduled_time)::time, 'HH24:MI:SS')
+                WHEN upper(btrim(scheduled_time)) ~ '^[0-9]{1,2}:[0-9]{2}:[0-9]{2} (AM|PM)$'
+                    THEN to_char(to_timestamp(upper(btrim(scheduled_time)), 'HH12:MI:SS AM')::time, 'HH24:MI:SS')
+                WHEN upper(btrim(scheduled_time)) ~ '^[0-9]{1,2}:[0-9]{2} (AM|PM)$'
+                    THEN to_char(to_timestamp(upper(btrim(scheduled_time)), 'HH12:MI AM')::time, 'HH24:MI:SS')
+                WHEN upper(btrim(scheduled_time)) ~ '^[0-9]{1,2} (AM|PM)$'
+                    THEN to_char(to_timestamp(upper(btrim(scheduled_time)), 'HH12 AM')::time, 'HH24:MI:SS')
+                ELSE '09:00:00'
+            END
+            """
+        )
 
 
 class Migration(migrations.Migration):
